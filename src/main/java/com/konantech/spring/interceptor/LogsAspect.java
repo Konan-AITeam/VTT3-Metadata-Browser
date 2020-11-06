1771fba22c3ddcae148fe4e422d07f3e311c25b1
konan-LKS <74031857+konan-LKS@users.noreply.github.com>
2020-11-06 11:26:54 +0900

    Update LogsAspect.java

--- src/main/java/com/konantech/spring/interceptor/LogsAspect.java
+++ src/main/java/com/konantech/spring/interceptor/LogsAspect.java
@@ -12,7 +12,6 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-
 @Component
 @Aspect
 public class LogsAspect {

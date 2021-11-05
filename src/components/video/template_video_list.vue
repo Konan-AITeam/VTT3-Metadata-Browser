3a8fbae2881c54eedbd71ebd1035efaf3c19e2ce
sorajung <sora.jung@konantech.com>
2021-11-05 14:36:02 +0900

    video list 수정

---
+++ src/components/video/template_video_list.vue
@@ -0,0 +1,43 @@
+<template>
+  <tr v-for="(video, idx) in view_videos" v-bind:key="video.path">
+    <td>{{ (page - 1) * limit + (idx+1)}}</td>
+    <td>{{ video.key }}</td>
+    <td class="videoName">
+      <router-link :to="{name: 'Shot', params: {'videoId' : video.key}}">{{ video.fileName }}</router-link>
+    </td>
+  </tr>
+  <tr v-show="isShow">
+    <td colspan="3">검색 결과가 없습니다.</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'template_video_list',
+  props: {
+    view_videos: Object,
+    total: Number,
+    limit: Number,
+    page: Number
+  },
+  watch: {
+    view_videos: function () {
+      if (Object.keys(this.view_videos).length === 0) {
+        this.isShow = true
+      } else {
+        this.isShow = false
+      }
+    }
+  },
+  data () {
+    return {
+      isShow: false
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
